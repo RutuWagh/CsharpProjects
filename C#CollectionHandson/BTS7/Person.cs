df7c4077f75894993e2ae50fb2b3b0cc542d7a73
RutuWagh <rutuwagh06@gmail.com>
2021-12-31 12:00:53 +0530

    C#CollectionHandson

---
+++ C#CollectionHandson/BTS7/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTS7
+{
+    class Person
+    {
+        public string Name;
+       public int Age;
+       // public double Height;
+        
+        public override string ToString()
+        {
+            return Name + " - " + Age ;
+        }
+
+    }
+}
---
+++ C#CollectionHandson/BTS7/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BTS7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            Console.WriteLine("---- Input -----");
+            Stack s = new Stack();
+            Queue q = new Queue();
+            int total = 3;
+            for (int i = 0; i < total; i++)
+            {
+                Console.Write("Person Name : ");
+                string name = Console.ReadLine();
+                Console.Write("Person Age : ");
+                int age = Convert.ToInt32(Console.ReadLine());
+              //  Console.Write("Person's Height : ");
+               // double height = Convert.ToDouble(Console.ReadLine());
+                list.Add(new Person()
+                {
+                    Name = name,
+                    Age = age
+                  //  Height = height
+                });
+                s.Push(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+                q.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+
+            }
+
+            Console.WriteLine("---- ListOutput ----");
+            foreach (Person a in list)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            Console.ReadLine();
+            Console.WriteLine("----- StackOutput -----");
+            for(int i = 0; i< total; i++)
+            {
+                Person p = (Person)s.Pop();
+                Console.WriteLine(p.ToString());
+            }
+            Console.ReadLine();
+            Console.WriteLine("----- QueueOutput -----");
+            /* foreach(Person f in q)
+             {
+                 Console.WriteLine(f.ToString());
+             }
+             Console.ReadLine();*/
+            for (int i = 0; i < total; i++)
+            {
+                Person p1 = (Person)q.Dequeue();
+                Console.WriteLine(p1.ToString());
+            }
+        }
+
+        /*
+        D d1= new D { Name = "John", Age = 12 };
+        Console.WriteLine(d1);*/
+    }
+}
+
+    
+
+
