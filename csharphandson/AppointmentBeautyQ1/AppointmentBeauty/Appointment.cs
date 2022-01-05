fd6f36a6c5306f406bb98850447acc509db4030a
RutuWagh <rutuwagh06@gmail.com>
2022-01-05 09:32:48 +0530

    csharphandson beauty_appointment & Car,Assemble Uploaded

---
+++ csharphandson/AppointmentBeautyQ1/AppointmentBeauty/Appointment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBeauty
+{
+   static class Appointment
+   {
+        public static void Schedule()
+        {
+
+        }
+        public static void HasPassed()
+        {
+
+        }
+        public static void IsAfternoonAppointment()
+        {
+
+        }
+        public static void Description()
+        {
+
+        }
+        public static void AnniversaryDate()
+        {
+
+        }
+   }
+}
+
+   
